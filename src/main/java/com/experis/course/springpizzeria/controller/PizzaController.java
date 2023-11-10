0af152af531581724762c65b6bf09a4ed1b384c7
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-11-10 15:30:09 +0100

    add show and message error

--- src/main/java/com/experis/course/springpizzeria/controller/PizzaController.java
+++ src/main/java/com/experis/course/springpizzeria/controller/PizzaController.java
@@ -3,12 +3,16 @@ package com.experis.course.springpizzeria.controller;
 import com.experis.course.springpizzeria.model.Pizza;
 import com.experis.course.springpizzeria.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/pizze")
@@ -21,5 +25,16 @@ public class PizzaController {
         List<Pizza> pizzaList = pizzaRepository.findAll();
         model.addAttribute("pizzaList", pizzaList);
         return "pizze/list";
+    }
+
+    @GetMapping("/show/{id}")
+    public String show(@PathVariable Integer id, Model model) {
+        Optional<Pizza> result = pizzaRepository.findById(id);
+        if (result.isPresent()) {
+            model.addAttribute("pizza", result.get());
+            return "pizze/show";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "pizza with id " + id + "not found");
+        }
     }
 }
